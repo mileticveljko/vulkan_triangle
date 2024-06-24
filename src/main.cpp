ae15a51d03aa2d575e4b81c4ec10549d2cd60493
Dzo <mileticveljko99@gmail.com>
2024-06-25 00:09:11 +0200

    Creating glfw window for vulkan

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+#define UNUSED(x) (void)x
+
+int main(int argc, char* argv[])
+{
+	UNUSED(argc);
+	UNUSED(argv);
+
+  	if(!glfwInit())
+    {
+    	return 1;
+    }
+
+    if(!glfwVulkanSupported())
+   	{
+    	return 1;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Triangle",
+                                          nullptr, nullptr);
+
+	if(!window)
+	{
+		glfwTerminate();
+		return 1;
+	}
+
+    while (!glfwWindowShouldClose(window))
+    {
+  		glfwPollEvents();  	
+    }
+
+	glfwTerminate();
+  
+	return 0;
+}
